ca3e19721044644cae983d81ea20f01fedef6c14
Iury Vasconcelos <iury0393@gmail.com>
2020-06-28 16:01:02 -0300

    Deletando casos

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -22,6 +22,20 @@ export default function Profile() {
     })
   }, [ongId]);
 
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId,
+        }
+      });
+
+      setIncidents(incidents.filter(incident => incident.id !== id));
+    } catch (err) {
+      alert('Erro ao deletar caso. Tente novamente.');
+    }
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -45,9 +59,9 @@ export default function Profile() {
             <p>{incident.description}</p>
 
             <strong>VALOR:</strong>
-            <p>{incident.value}</p>
+            <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL'}).format(incident.value)}</p>
 
-            <button type="button">
+            <button onClick={() => handleDeleteIncident(incident.id)} type="button">
               <FiTrash size={20} color="#a8a8b3" />
             </button>
           </li>
